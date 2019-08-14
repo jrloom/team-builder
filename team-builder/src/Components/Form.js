d217d630bf9c0bfa4985a1b8ca2627106796c778
Jason Loomis <jason@jrloomis.com>
2019-08-13 21:52:19 -0400

    added change and submit functionality

--- team-builder/src/Components/Form.js
+++ team-builder/src/Components/Form.js
@@ -7,18 +7,36 @@ const Form = props => {
     role: []
   });
 
+  const handleChange = event => {
+    setTeamMember({ ...teamMember, [event.target.name]: event.target.value });
+  };
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    const newTeamMember = {
+      ...teamMember,
+      id: Date.now()
+    };
+    props.addNewTeamMember(newTeamMember);
+    setTeamMember({
+      name: "",
+      email: "",
+      role: []
+    });
+  };
+
   return (
     <div>
       <h1>Come Join Us</h1>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label htmlFor="name">Name:</label>
-        <input type="text" name="name" />
+        <input type="text" name="name" value={teamMember.name} onChange={handleChange} />
 
         <label htmlFor="email">Email:</label>
-        <input type="email" name="email" />
+        <input type="email" name="email" value={teamMember.email} onChange={handleChange} />
 
         <label htmlFor="role">Role:</label>
-        <input type="text" name="role" />
+        <input type="text" name="role" value={teamMember.role} onChange={handleChange} />
 
         <button type="submit">Become</button>
       </form>
