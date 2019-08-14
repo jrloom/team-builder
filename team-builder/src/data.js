c55308dc622c6a5843f1999f8c5dec690f779e20
Jason Loomis <jason@jrloomis.com>
2019-08-13 22:33:48 -0400

    added dummy data for team member roles

--- team-builder/src/data.js
+++ team-builder/src/data.js
@@ -6,3 +6,26 @@ export const teamMemberData = [
     role: "Sage"
   }
 ];
+
+export const teamMemberRoles = [
+  {
+    id: 0,
+    role: ""
+  },
+  {
+    id: 1,
+    role: "Sage"
+  },
+  {
+    id: 2,
+    role: "Jedi"
+  },
+  {
+    id: 3,
+    role: "Wizard"
+  },
+  {
+    id: 4,
+    role: "Gopher"
+  }
+];
